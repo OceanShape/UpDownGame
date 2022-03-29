82d516d10da82d07310602a459686f327262b79c
OceanShape <oceanshape700@gmail.com>
2022-03-29 23:19:30 +0900

    feat: Add ViewController.sliderValueChanged()

--- UpDown/UpDown/ViewController.swift
+++ UpDown/UpDown/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
 
 }
 
