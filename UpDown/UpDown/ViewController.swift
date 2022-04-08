473d6fbb32ab9d3b2d2bf63c4299461b691423ae
OceanShape <oceanshape700@gmail.com>
2022-04-08 23:43:07 +0900

    feat: Connect minimumValueLabel and maximumValueLabel

--- UpDown/UpDown/ViewController.swift
+++ UpDown/UpDown/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var tryCountLabel: UILabel!
     @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
