e90315823eb23f958079d6285e973d9d96d4b5d4
OceanShape <oceanshape700@gmail.com>
2022-04-09 02:01:49 +0900

    feat: Complete ViewController.reset()

--- UpDown/UpDown/ViewController.swift
+++ UpDown/UpDown/ViewController.swift
@@ -17,29 +17,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var sliderValueLabel: UILabel!
     @IBOutlet weak var minimumValueLabel: UILabel!
     @IBOutlet weak var maximumValueLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb.png"), for: .normal)
         reset()
     }
-
+    
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
     }
-
+    
     @IBAction func touchUpHitButton(_ sender: UIButton) {
         print(slider.value)
     }
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
-        print("touch up reset button")
         reset()
     }
     
     func reset() {
-        print("reset!")
+        randomValue = Int.random(in: 0...30)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
     }
     
 }
