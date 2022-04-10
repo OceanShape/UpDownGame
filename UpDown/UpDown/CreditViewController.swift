b5bae3d19b4686e4e61f859b3f39dcf68b1fff5d
OceanShape <oceanshape700@gmail.com>
2022-04-10 18:41:01 +0900

    feat: Add and connect touchUpDismissButton()

--- UpDown/UpDown/CreditViewController.swift
+++ UpDown/UpDown/CreditViewController.swift
@@ -9,9 +9,8 @@ import UIKit
 
 class CreditViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
-
+    
 }
